29abc1699eb590d542d4407af38abed77cbec9c1
Martin Voigt <m.voigt@ai4bd.com>
2017-12-12 14:42:54 +0100

    changed paths and libs but did not work

--- package/scripts/common.py
+++ package/scripts/common.py
@@ -54,13 +54,13 @@ def download_hue():
   """
   Download Hue to the installation directory
   """
-  Execute('export {http,https}_proxy={}'.format('http://10.30.76.11:3128'))
-  Execute('{0} | xargs wget -O hue.tgz'.format(params.download_url))
-  Execute('tar -zxvf hue.tgz -C {0} && rm -f hue.tgz'.format(params.hue_install_dir))
+  Execute('{0} | xargs wget -O {1}/hue.tgz'.format(params.download_url,params.hue_install_dir))
+  Execute('tar -zxvf {0}/hue.tgz -C {0} && rm -f {0}/hue.tgz'.format(params.hue_install_dir))
+  Execute('rm -rf {0}/hue'.format(params.hue_install_dir))
+  Execute('cd {0}/hue-* && make install > /var/tmp/hue_install.log'.format(params.hue_install_dir))
+  Execute('rm -rf {0}/hue-*'.format(params.hue_install_dir))
   # Ensure all Hue files owned by hue
-  Execute('mv {0}/{1} {2}'.format(params.hue_install_dir, params.hue_version_dir, params.hue_dir))
   Execute('chown -R {0}:{1} {2}'.format(params.hue_user,params.hue_group,params.hue_dir))
-  Execute('{0}/make install'.format(params.hue_dir))
   Execute('ln -s {0} /usr/hdp/current/hue-server'.format(params.hue_dir))
   Logger.info("Hue Service is installed")
 
